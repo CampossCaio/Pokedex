0b8673cdea929fd1770b3fb0abd4eae8e6352bf0
CampossCaio <campos.f.caio@gmail.com>
2020-05-18 23:57:09 -0300

    Versão 1.0

--- src/App.js
+++ src/App.js
@@ -1,25 +1,23 @@
 import React from 'react';
-import logo from './logo.svg';
+import ApolloCliente from 'apollo-boost';
+import { ApolloProvider } from '@apollo/react-hooks';
 import './App.css';
 
+import PokemonContainer from './containers/PokemonContainer';
+
+
 function App() {
+
+  const client = new ApolloCliente({
+    uri: 'https://graphql-pokemon.now.sh'
+  });
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ApolloProvider client={client}>
+      <main>
+          <PokemonContainer/>
+      </main>
+    </ApolloProvider>
   );
 }
 
